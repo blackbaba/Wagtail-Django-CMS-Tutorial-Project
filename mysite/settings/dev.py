1186b3af285d5bf32e7f48852c21956a71dad391
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2021-07-19 19:32:02 +1000

    Wagtail official tutorial code

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-$3#9d2*n+q+=*=c88f_u1t$8zf$oht72ir942qlckr6gt(s15g'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
